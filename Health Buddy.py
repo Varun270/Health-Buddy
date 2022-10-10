4948c449827a1e2c3e15876a903c8e78e0f53205
Varun Shrivastava <varunshrivastava2706@gmail.com>
2022-10-10 10:34:16 +0530

    1.Corrected structure of code 2. Added get_data() 3.Hide API keys 4. Added .gitignore

--- Health Buddy.py
+++ Health Buddy.py
@@ -5,6 +5,8 @@ import json
 import os
 import re
 import requests
+from dotenv import load_dotenv
+load_dotenv()
 tess.pytesseract.tesseract_cmd = r'C:\Users\Varun Shrivastava\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 import string
 import cv2
@@ -20,52 +22,41 @@ f.close()
 
 
 # Removing punctuations and fetching data for ingrediants
-with open("Ingrediants.txt") as f:
-    text = f.read()
-    words = text.split(",")
-    table = str.maketrans("", "", string.punctuation)
-    stripped_words = [w.translate(table) for w in words]
+def get_data(filename="Ingrediants.txt"):
+    with open(filename) as f:
+        text = f.read()
+        words = text.split(",")
+        table = str.maketrans("", "", string.punctuation)
+        stripped_words = [w.translate(table) for w in words]
 
-    for word in stripped_words:
-        if word[:11].lower() == "ingredients":
-            improved_str.append(word[11:].replace('\n', '').replace('\x0c', ''))
+        for word in stripped_words:
+            if word[:11].lower() == "ingredients":
+                improved_str.append(word[11:].replace('\n', '').replace('\x0c', ''))
 
-        else:
-            improved_str.append(word.replace('\n', '').replace('\x0c', ''))
+            else:
+                improved_str.append(word.replace('\n', '').replace('\x0c', ''))
 
+        for word in list(set(improved_str)):
+            url = "https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
 
-    print(list(set(improved_str)))
-    for word in list(set(improved_str)):
-        url = "https://edamam-food-and-grocery-database.p.rapidapi.com/parser"
+            querystring = {"ingr": word}
+            headers = {
+                "X-RapidAPI-Key": os.getenv("API_KEY"),
+                "X-RapidAPI-Host": os.getenv("Host")
+            }
+            response = requests.request("GET", url, headers=headers, params=querystring)
 
-        querystring = {"ingr": word}
+            file = open("fetched_data.json", 'w')
+            json.dump(response.json(), file)
+        file.close()
 
-        headers = {
-            "X-RapidAPI-Key": "fd93ec0010msh8fc5131067ae223p1c8310jsn02a3f8bb78fd",
-            "X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com"
-        }
-
-        response = requests.request("GET", url, headers=headers, params=querystring)
-
-        # url = "https://food-ingredients-by-pti.p.rapidapi.com/api/1.0/FoodIngredients/GetIngredientDetails"
-        #
-        # querystring = {"ingredientName": word}
-        #
-        # headers = {
-        #     "Content-Type": "application/json",
-        #     "X-RapidAPI-Key": "fd93ec0010msh8fc5131067ae223p1c8310jsn02a3f8bb78fd",
-        #     "X-RapidAPI-Host": "food-ingredients-by-pti.p.rapidapi.com"
-        # }
-        #
-        # response = requests.request("GET", url, headers=headers, params=querystring)
+    # fileVariable = open('fetched_data.json', 'r+')
+    # fileVariable.truncate(0)
+    # fileVariable.close()
 
 
-
-        file = open("fetched_data.json", 'w')
-        json.dump(response.json(), file)
-    file.close()
-
-
-
-
-f.close()
+if __name__ == '__main__':
+    get_data()
+    # fileVariable = open('Ingrediants.txt', 'r+')
+    # fileVariable.truncate(0)
+    # fileVariable.close()
